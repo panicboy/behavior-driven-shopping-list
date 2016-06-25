dbfad141cdd741be986cbf5a5d9ee862713abe51
panicboy <asasaki@gmail.com>
2016-06-25 10:21:09 -1000

    first 5 tests of ShoppiungListItem written & passing

--- js/ShoppingListItem.js
+++ js/ShoppingListItem.js
@@ -1,9 +1,22 @@
 function ShoppingListItem(name, description) {
   this._name = name;
   this._description = description;
+  var is_done = false;
+
+  function _check (){
+    is_done = true;
+  }
+
+  function _unheck (){
+    is_done = false;
+  }
+
   return {
   name: this._name,
-  description: this._description
+  description: this._description,
+  is_done: is_done,
+  // check: _check,
+  // uncheck: _uncheck
   };
 }
 
--- js/test/ShoppingList.spec.js
+++ js/test/ShoppingList.spec.js
@@ -14,21 +14,36 @@ describe('ShoppingListItem', function(){
     shoppinglistitem = new ShoppingListItem('Fish','A lovely but dangerous goldfish.');
   });
 
-  it('should be a Class definition', function() {
+  it(`should be a Class definition`, function() {
     ShoppingListItem.should.be.a.function;
   });
 
-  it('should have a property name', function() {
+  it('should return an object literal when invoked', function () {
+    (ShoppingListItem()).should.be.an('object');
+  });
+
+  it(`should have a property 'name'`, function() {
     shoppinglistitem.name.should.be.equal('Fish');
   });
 
-  it('should have a property description', function() {
+  it(`should have a property 'description'`, function() {
     shoppinglistitem.description.should.be.equal('A lovely but dangerous goldfish.');
   });
 
+  it(`should have a property 'is_done'`, function() {
+    shoppinglistitem.is_done.should.be.equal(false);
+  });
+
+  // describe('Shopping List Item methods', function() {
+  //   describe('#check', function() {
+
+  //   }
+  // });
+
+  //   //end of shoppingListItem method tests
+  // });
+
+//end of shoppingListItem tests
 });
-  //ShoppingListItem has a property named `name`
-  //ShoppingListItem has a property named `description`
-  //ShoppingListItem has a property named `is_done`
   //ShoppingListItem has a constructor method that accepts 2 arguments, `name` and `description`
     //the constructor method sets the new instances `name` and `description` properties using the arguments passed in
